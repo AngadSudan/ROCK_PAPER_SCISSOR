78e474c20162053a72fda543463dcb4fcebb8f91
AngadSudan <angadsudan453@gmail.com>
2024-06-13 23:51:32 +0530

    added the game page

---
+++ gamelogic.js
@@ -0,0 +1,8 @@
+let rock= document.querySelector(".rock");
+let paper= document.querySelector(".paper");
+let scissor= document.querySelector(".scissor");
+let multi= document.querySelector(".multi");
+let single= document.querySelector(".single");
+let win=0;
+let loss=0;
+let total_game=0;
